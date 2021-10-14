a4e96d746df4ca1ecb76bde9f8fafa7d63d617a8
Rickard <33436048+Ignalina@users.noreply.github.com>
2021-10-14 11:29:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,24 +12,22 @@ rm -rf schema-registry
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/kafka.git
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/common.git
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/rest-utils.git
-ls -l 
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/schema-registry.git
 
-pushd kafka
+cd kafka
 ./gradlew jar
 ./gradlewAll publishToMavenLocal
-popd
+cd..
 
-
-pushd common
+cd common
 mvn clean install  -Dmaven.test.skip=true 
-popd
+cd..
 
-pushd rest-utils
+cd rest-utils
 mvn clean install  -Dmaven.test.skip=true 
-popd
+cd..
 
-pushd schema-registry
+cd schema-registry
 mvn clean install  -Dmaven.test.skip=true 
-popd
+cd..
 
