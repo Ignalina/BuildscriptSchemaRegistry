bfb1ce8b575b75a7c72686a9882215991a88705a
Rickard <33436048+Ignalina@users.noreply.github.com>
2021-10-14 11:17:08 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,14 @@
 #
 # Dependencies: bash , mvn
 
+rm -rf kafka
+rm -rf rest-utils
+rm -rf common
+rm -rf schema-registry
+
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/kafka.git
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/common.git
-git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/rest-util.git
+git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/rest-utils.git
 ls -l 
 git clone -b 6.2.1-post  --depth 1  https://github.com/confluentinc/schema-registry.git
 
@@ -17,16 +22,14 @@ popd
 
 
 pushd common
-#mvn clean install  -Dmaven.test.skip=true 
+mvn clean install  -Dmaven.test.skip=true 
 popd
 
-pushd rest-util
-#mvn clean install  -Dmaven.test.skip=true 
+pushd rest-utils
+mvn clean install  -Dmaven.test.skip=true 
 popd
 
 pushd schema-registry
-#mvn clean install  -Dmaven.test.skip=true 
+mvn clean install  -Dmaven.test.skip=true 
 popd
-
-
 
